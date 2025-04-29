8189b6c752c06989c0da071e04920b9e17f10d9e
Maurdekye <ncolaprete@gmail.com>
2025-04-30 00:14:38 +0300

    work counter & sentinels

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(mpmc_channel)]
 use std::{
-    sync::mpmc::channel,
+    sync::{atomic::{AtomicBool, AtomicUsize, Ordering}, mpmc::channel},
     thread::{self, available_parallelism},
 };
 
@@ -10,6 +10,8 @@ where
     I::Item: Send + Sync,
     F: Fn(I::Item, I::Item) -> I::Item + Send + Sync,
 {
+    let in_flight = AtomicUsize::new(0);
+    let work_finished = AtomicBool::new(false);
     thread::scope(|scope| {
         let (outbox, inbox) = channel();
         let num_cpus: usize = available_parallelism().map(usize::from).unwrap_or(1);
@@ -18,22 +20,34 @@ where
                 let inbox = inbox.clone();
                 let outbox = outbox.clone();
                 let f = &f;
+                let in_flight = &in_flight;
+                let work_finished = &work_finished;
                 scope.spawn(move || {
                     let mut stock = None;
-                    for elem in inbox {
+                    while let Some(elem) = inbox.recv().unwrap() {
                         stock = match stock {
                             Some(other) => {
-                                outbox.send(f(elem, other)).unwrap();
+                                outbox.send(Some(f(elem, other))).unwrap();
                                 None
                             }
                             None => Some(elem),
+                        };
+                        let in_flight = in_flight.fetch_sub(1, Ordering::SeqCst);
+                        let work_finished = work_finished.load(Ordering::SeqCst);
+                        if work_finished && in_flight <= 1 {
+                            (1..num_cpus).for_each(|_| outbox.send(None).unwrap());
+                            break;
                         }
                     }
                     stock
                 })
             })
             .collect();
-        it.for_each(|elem| outbox.send(elem).unwrap());
+        it.for_each(|elem| {
+            in_flight.fetch_add(1, Ordering::SeqCst);
+            outbox.send(Some(elem)).unwrap()
+        });
+        work_finished.store(true, Ordering::SeqCst);
         drop((inbox, outbox));
         workers
             .into_iter()
